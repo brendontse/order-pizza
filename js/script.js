5e9ee2261b398cc46c82fd125fd1dc1731a04355
Brendon <brendonjtse@gmail.comgit config --global user.email brendonjtse@gmail.com>
2019-08-30 11:08:46 -0700

    add size cost calculator to pizza protoype

--- js/script.js
+++ js/script.js
@@ -1,9 +1,21 @@
-function Pizza(size, crust, cheese, meat) {
+function Pizza(size, crust, cheese, meat) { // make a pizza
   this.size = size,
     this.crust = crust,
     this.cheese = cheese,
     this.meat = meat,
     this.price;
+}
+
+Pizza.prototype.getprice = function() { // calculate the cost of a specific pizza
+  this.price = 5; //base cost
+  if (this.size === 'S') { //size cost
+    this.price = this.price * 0.75;
+  } else if (this.size === 'L') {
+    this.price = this.price * 1.25;
+  }
+
+
+}
 }
 
 document.ready(function() {
