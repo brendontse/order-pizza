fcdc862012361905765ff347df928b7f80d9d24d
Brendon <brendonjtse@gmail.comgit config --global user.email brendonjtse@gmail.com>
2019-09-02 22:03:20 -0700

    fix getPrice prototype operators

--- js/script.js
+++ js/script.js
@@ -1,23 +1,23 @@
 function Pizza(size, crust, cheese, meat) { // make a pizza
-  this.size = size,
-    this.crust = crust,
-    this.cheese = cheese,
-    this.meat = meat,
-    this.price;
+  this.size = size;
+  this.crust = crust;
+  this.cheese = cheese;
+  this.meat = meat;
+  this.price;
 }
 
-Pizza.prototype.getprice = function() { // calculate the cost of a specific pizza
+Pizza.prototype.getPrice = function() { // calculate the cost of a specific pizza
   this.price = 5; // base cost
   if (this.size === 'S') { // size cost
-    this.price = this.price * 0.75;
+    this.price *= 0.75;
   } else if (this.size === 'L') {
-    this.price = this.price * 1.25;
+    this.price *= 1.25;
   }
 
   if (this.crust === 'thiCC' || 'gluten-free') { // crust cost
-    this.price = this.price * 1.20;
+    this.price *= 1.2;
   } else if (this.crust === 'thin' || 'flatbread') {
-    this.price = this.price * 1.1;
+    this.price *= 1.1;
   }
 
   if (this.cheese === 'cheddar') { // cheese cost
@@ -26,7 +26,7 @@ Pizza.prototype.getprice = function() { // calculate the cost of a specific pizz
     this.price += 2;
   }
 
-  if (this.meat === 'every kind of meat') { // meat cost
+  if (this.meat === 'all-meat') { // meat cost
     this.price += 4;
   } else if (this.meat === 'bacon' || 'chicken' || 'sausage') {
     this.price += 2;
